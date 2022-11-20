604617b572c96681c97aace84f1c7a033956e7c9
FreeWall <info@michalvanek.net>
2022-11-20 10:09:03 +0100

    breadcrum icon

--- src/components/layout/main/page/breadcrumbs.tsx
+++ src/components/layout/main/page/breadcrumbs.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
-import { ReactElement } from 'react';
+import { memo, ReactElement } from 'react';
+import AngleDoubleRightIcon from './../../../icons/angle_double_right.svg';
+const MemoAngleDoubleRightIcon = memo(AngleDoubleRightIcon);
 
 export interface BreadcrumbsProps {
   breadcrumbs: {
@@ -14,14 +16,20 @@ export default function Breadcrumbs(props: BreadcrumbsProps) {
       {props.breadcrumbs.map((breadcrumb, idx) => (
         <div
           key={idx}
-          className="mr-2 flex"
+          className="mr-[10px] flex items-center"
         >
           <Link href={breadcrumb.link}>
-            <a className="inline cursor-pointer py-[2px] hover:text-dbfy-link-color hover:underline">
+            <a className="inline cursor-pointer py-[2px] text-sm hover:text-dbfy-link hover:underline">
               {breadcrumb.name}
             </a>
           </Link>
-          {idx < props.breadcrumbs.length - 1 && <div className="ml-2 py-[2px]">{'>'}</div>}
+          {idx < props.breadcrumbs.length - 1 && (
+            <div className="ml-[10px] py-[2px]">
+              <div className="h-[9px] w-[9px] fill-dbfy-dark-icon">
+                <MemoAngleDoubleRightIcon />
+              </div>
+            </div>
+          )}
         </div>
       ))}
     </div>
