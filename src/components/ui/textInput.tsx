02ec52172af1aa110c28d236a71fc3ddb1af8d42
FreeWall <info@michalvanek.net>
2022-11-21 21:52:41 +0100

    rename textInput

---
+++ src/components/ui/textInput.tsx
@@ -0,0 +1,18 @@
+import { ForwardedRef, forwardRef } from 'react';
+
+export interface TextInputProps {
+  type: 'text' | 'password';
+  placeholder?: string;
+  value?: string;
+}
+
+export default forwardRef(function TextInput(props: TextInputProps, ref: ForwardedRef<HTMLInputElement>) {
+  return (
+    <input
+      ref={ref}
+      type={props.type}
+      className="w-full rounded-[3px] border border-dbfy-border py-[6px] px-2"
+      {...{ placeholder: props.placeholder, defaultValue: props.value }}
+    />
+  );
+});
