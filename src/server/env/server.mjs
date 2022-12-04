971e93efd8f4b5d61708fac48d663f04b542831e
FreeWall <info@michalvanek.net>
2022-12-04 17:00:41 +0100

    build dir

--- src/server/env/server.mjs
+++ src/server/env/server.mjs
@@ -7,5 +7,5 @@ if (!fs.existsSync(envFile)) {
   let content = fs.readFileSync('.env.example');
   content = content.toString().replace(/IRONSESSION_SECRET=.*/, 'IRONSESSION_SECRET=' + nanoid(32));
   fs.writeFileSync(envFile, content);
-  process.exit();
+  //process.exit();
 }
